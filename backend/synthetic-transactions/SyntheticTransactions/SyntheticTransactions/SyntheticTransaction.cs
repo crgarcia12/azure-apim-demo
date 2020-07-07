297195aac6c832365def8bcb2066e2319cd8d341
Carlos Garcia Lalicata <crgarcia@live.com.ar>
2020-07-07 12:14:10 +0200

    init

--- backend/synthetic-transactions/SyntheticTransactions/SyntheticTransactions/SyntheticTransaction.cs
+++ backend/synthetic-transactions/SyntheticTransactions/SyntheticTransactions/SyntheticTransaction.cs
@@ -9,23 +9,22 @@ namespace SyntheticTransactions
 {
     public static class SyntheticTransaction
     {
+        const string Version = "6";
+        private static HttpClient httpClient = new HttpClient();
+
+
         [FunctionName(nameof(SyntheticTransaction))]
         public static async Task Run([TimerTrigger("0 */2 * * * *")]TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation($"{nameof(SyntheticTransaction)} trigger function v5 executed at: '{DateTime.Now}'");
+            log.LogInformation($"{nameof(SyntheticTransaction)} trigger function v{Version} executed at: '{DateTime.Now}'");
 
             string endpoint = Environment.GetEnvironmentVariable("ApplicationEndpoint");
 
-            using (HttpClient client = new HttpClient())
+            for (int i = 0; i < 100; i++)
             {
-                for (int i = 0; i < 100; i++)
-                {
-                    string response = await client.GetStringAsync(endpoint);
-                    log.LogInformation($"{nameof(SyntheticTransaction)} responded '{endpoint}' trigger '{i}': '{response}'");
-                }
+                string response = await httpClient.GetStringAsync(endpoint);
+                log.LogInformation($"{nameof(SyntheticTransaction)} responded '{endpoint}' trigger '{i}': '{response}'");
             }
-
-
         }
     }
 }
